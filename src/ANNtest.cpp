bfdc731c081b083d908a3f4ae7e4687d5e22738a
Julian Anthony Brackins <julian.brackins@mines.sdsmt.edu>
2016-02-09 17:57:55 -0700

    Updated the doxygen header for ANNtest.cpp because apparently doxygen thinks that's the main one. This was done for doxygen html purposes so that the website stuff gets properly created

--- src/ANNtest.cpp
+++ src/ANNtest.cpp
@@ -23,7 +23,17 @@
  * @section program_section Program Information
  *
  * @details
- * Program Info
+ * TINDER stands for Tree Intelligence Network for Determining Ember Risk and 
+ * is, to our knowledge, probably the first software program to ever have this 
+ * name. Any other similarities in name are purely coincidental and are in no 
+ * relation to the TINDER project and its morals or values.
+ *
+ * TINDER is an artificial neural network that utilizes a back-propagation 
+ * network to determine a given year's fire severity risk based on PDSI 
+ * (Palmer Drought Severity Index) data. This program is strictly for 
+ * educational purposes for our academic understanding of how a neural network 
+ * is designed and how it operates. This program in no way ensures 100% accurate 
+ * fire predictions and should not be held to such a standard.
  *
  * @section compile_section Compiling and Usage
  *
@@ -32,7 +42,7 @@
  *
  * @par Usage:
  @verbatim
- ./ANNtest <parameterfile>zz
+ ./ANNtest <parameterfile>
  @endverbatim
  *
  * @section todo_bugs_modification_section Todo, Bugs, and Modifications
@@ -41,8 +51,10 @@
  @verbatim
  Date              Modification
  ----------------  --------------------------------------------------------------
- January 27, 2016  * Began project.
- * 
+ January  27, 2016  * Began project.
+ February 03, 2016  * Prm Class started for reading/writing .prm parameter files
+ February 06, 2016  * Finished basic setter methods for Prm Class
+ February 09, 2016  * Finished getter, writing and printing methods for Prm Class
  @endverbatim
  *
  ******************************************************************************/
